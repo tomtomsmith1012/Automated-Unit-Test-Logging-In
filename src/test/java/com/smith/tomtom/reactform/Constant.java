07ae2aea2f94621ff12aa293321a2d4b99c652ce
TomSmith <tomtomsmith1012@gmail.com>
2019-01-25 10:51:58 +0000

    first commit

---
+++ src/test/java/com/smith/tomtom/reactform/Constant.java
@@ -0,0 +1,22 @@
+package com.smith.tomtom.reactform;
+
+public class Constant {
+
+	public static final String CHROMEDRIVER_PATH = "C:\\LocalInstall\\chromedriver.exe";
+	
+	public static final String REPORT_PATH = "C:\\Users\\Admin\\Documents\\QA_BAE\\HTML\\TestReports\\report.html";
+	
+	public static final String START_URL = "http://192.168.1.105:3000/";
+	
+	public static final String FINAL_CHECK = "//*[@id=\"root\"]/div/div/h1";
+	
+	public static final String PASSWORD_CHECK = "//*[@id=\"root\"]/div/div/p[10]";
+	
+	public static final String[] emails = {"jh@juhg.com", "ygf@df.com", "wef@aef.com", "wer@fwef.com"};
+	
+	public static final String[] passwords = {"ww3£www", "tt3£tt", "pp£3pp", "q3£2WW"};
+	
+	public static final String[] wrongPasswords = {"tytyty", "67tydf", "rt5rt4", "we34kj"};
+	
+	public static final int[] countryIds = {0, 0, 1, 2};
+}
---
+++ src/test/java/com/smith/tomtom/reactform/ReactFormTest.java
@@ -0,0 +1,104 @@
+package com.smith.tomtom.reactform;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ReactFormTest {
+
+	WebDriver driver;
+	static ExtentReports report;
+	ExtentTest test;
+
+	@BeforeClass
+	public static void firstStart() {
+		report = new ExtentReports(Constant.REPORT_PATH, true);
+	}
+
+	@AfterClass
+	public static void finalClose() {
+		report.flush();
+	}
+
+	@Before
+
+	public void startUp() {
+		System.setProperty("webdriver.chrome.driver", Constant.CHROMEDRIVER_PATH);
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void shutDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void webTest() {
+
+		int testNumber = 1;
+		for (int i = 0; i < 4; i++) {
+			test = report.startTest("Correct password log in test " + testNumber + ".");
+			testNumber++;
+			LandingPage landing = PageFactory.initElements(driver, LandingPage.class);
+
+			driver.get(Constant.START_URL);
+			test.log(LogStatus.INFO, "Navigating to react home page");
+
+			landing.fillForm(driver, Constant.emails[i], Constant.passwords[i], Constant.passwords[i],
+					Constant.countryIds[i]);
+			test.log(LogStatus.INFO, "Filling in form using email: " + Constant.emails[i] + ", password: "
+					+ Constant.passwords[i] + ".");
+
+			assertEquals("Login was not successful.", "Success!",
+					driver.findElement(By.xpath(Constant.FINAL_CHECK)).getText());
+			if (driver.findElement(By.xpath(Constant.FINAL_CHECK)).getText().equals("Success!")) {
+				test.log(LogStatus.PASS, "Login was successful.");
+			} else {
+				test.log(LogStatus.FAIL, "Login failed.");
+			}
+			report.endTest(test);
+		}
+	}
+
+	@Test
+	public void errorTest() {
+
+		int testNumber = 1;
+		for (int i = 0; i < 4; i++) {
+			test = report.startTest("Wrong password log in test " + testNumber + ".");
+			testNumber++;
+			LandingPage landing = PageFactory.initElements(driver, LandingPage.class);
+
+			driver.get(Constant.START_URL);
+			test.log(LogStatus.INFO, "Navigating to react home page");
+
+			landing.fillForm(driver, Constant.emails[i], Constant.passwords[i], Constant.wrongPasswords[i],
+					Constant.countryIds[i]);
+			test.log(LogStatus.INFO, "Filling in form using email: " + Constant.emails[i] + ", password: "
+					+ Constant.passwords[i] + ", confirm password: " + Constant.wrongPasswords[i] + ".");
+
+			assertEquals("Passwords do not match message not displayed.", "The passwords do not match",
+					driver.findElement(By.xpath(Constant.PASSWORD_CHECK)).getText());
+			if (driver.findElement(By.xpath(Constant.PASSWORD_CHECK)).getText().equals("The passwords do not match")) {
+				test.log(LogStatus.PASS, "Passwords do not match message was correctly displayed.");
+			} else {
+				test.log(LogStatus.FAIL, "Passwords do not match message was not correctly displayed.");
+			}
+		}
+		report.endTest(test);
+	}
+
+}
